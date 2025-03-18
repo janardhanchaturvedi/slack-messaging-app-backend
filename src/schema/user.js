3edd62f7d7a7eddae451e5fb24777765855e0a41
Janardhan <jan.cj84@gmail.com>
2025-03-18 23:45:32 +0530

    Added the basic user schema

---
+++ src/schema/user.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      match: [
+        //eslint-disable-next-line
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Pleas fill a valid email address'
+      ]
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+      unique: [true, 'Username is already present'],
+      match: [
+         
+        /[0-9]{6,15}[a-zA-Z]/,
+        'Username must contain only letters and numbers'
+      ]
+    },
+    avatar: {
+      type: String
+    }
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', function saveUser(next) {
+  const user = this;
+  user.avatar = `https://robohash.org/${user.username}`;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
