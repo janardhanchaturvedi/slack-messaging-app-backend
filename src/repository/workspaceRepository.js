336e7457cbeebf971934d12a37954db924e8aa06
Janardhan <jan.cj84@gmail.com>
2025-03-28 21:55:17 +0530

    feat : Implemented the get Workspace by name repository

--- src/repository/workspaceRepository.js
+++ src/repository/workspaceRepository.js
@@ -1,9 +1,24 @@
+import { StatusCodes } from 'http-status-codes';
 import Workspace from '../schema/workspace';
+import clientError from '../utils/errors/clientError';
 import crudRespository from './crudRepository';
 
 const workspaceRepository = {
   ...crudRespository,
-  getWorkspaceByName: async function () {},
+  getWorkspaceByName: async function (workspaceName) {
+    const workspace = await Workspace.findOne({
+      name: workspaceName
+    });
+
+    if (!workspace) {
+      throw new clientError({
+        explanation: 'Invalid data sent from the client',
+        message: 'Workspace not found',
+        statusCode: StatusCodes.NOT_FOUND
+      });
+    }
+    return workspace;
+  },
   getWorkspaceByJoinCode: async function () {},
   addMemberToWorkspace: async function () {},
   addChannelToWorkspace: async function () {},
