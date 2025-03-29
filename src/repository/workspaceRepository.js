f9951dd8f6087889dacd7bd63eddc6cc7eefbfde
Janardhan <jan.cj84@gmail.com>
2025-03-29 22:41:09 +0530

    feat (workspace) : Added the repo layer for the workspace

--- src/repository/workspaceRepository.js
+++ src/repository/workspaceRepository.js
@@ -19,8 +19,63 @@ const workspaceRepository = {
     }
     return workspace;
   },
-  getWorkspaceByJoinCode: async function () {},
-  addMemberToWorkspace: async function () {},
+  getWorkspaceByJoinCode: async function (joinCode) {
+    const workspace = await Workspace.findOne({
+      joinCode
+    });
+
+    if (!workspace) {
+      throw new clientError({
+        explanation: 'Invalid data send from the client',
+        mesaage: 'Workspace not found',
+        statusCode: StatusCodes.NOT_FOUND
+      });
+    }
+
+    return workspace;
+  },
+  addMemberToWorkspace: async function (workspaceId, memberId, role) {
+    const workspace = await Workspace.findById(workspaceId);
+
+    if (!workspace) {
+      throw new clientError({
+        explanation: 'Invalid data send from the client',
+        message: 'Workspace not found',
+        statusCode: StatusCodes.NOT_FOUND
+      });
+    }
+
+    const isValidUser = await Workspace.findById(memberId);
+
+    if (!isValidUser) {
+      throw new clientError({
+        explanation: 'Invalid data send from client',
+        message: 'User not found',
+        statusCode: StatusCodes.NOT_FOUND
+      });
+    }
+
+    const isMemberAlreadyPartOfWorkspace = workspace.members.find(
+      (member) => member.memberId === memberId
+    );
+
+    if (isMemberAlreadyPartOfWorkspace) {
+      throw new clientError({
+        explanation: 'Invalid data sent from the user',
+        mesaage: 'User already part of workspace',
+        statusCode: StatusCodes.FORBIDDEN
+      });
+    }
+
+    workspace.members.push({
+      memberId,
+      role
+    });
+
+    await workspace.save();
+
+    return workspace;
+  },
   addChannelToWorkspace: async function () {},
   fetchAllWorkspaceByMemberId: async function () {}
 };
